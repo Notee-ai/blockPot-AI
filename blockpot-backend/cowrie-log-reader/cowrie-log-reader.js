fdf7413822addbba3245e11cd37c6e7ca10b6eb4
anand <anandchowdary2005@gmail.com>
2025-04-22 16:39:42 +0530

    anand

---
+++ blockpot-backend/cowrie-log-reader/cowrie-log-reader.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+// Path to Cowrie JSON log file
+const logFilePath = '/full/path/to/cowrie/var/log/cowrie/cowrie.json'; // UPDATE THIS!
+
+const readStream = fs.createReadStream(logFilePath, {
+  encoding: 'utf8',
+  tail: true,
+});
+
+const rl = readline.createInterface({
+  input: readStream,
+});
+
+rl.on('line', (line) => {
+  try {
+    const logEntry = JSON.parse(line);
+    console.log('[COWRIE LOG]', logEntry);
+    // TODO: Trigger blockchain store or send to server.js
+  } catch (err) {
+    console.error('Invalid JSON:', line);
+  }
+});
